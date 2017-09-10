b1b53137d1cfabdd50ba4a5494512fdfa8d7fb00
Rik Watson <rik.watson@ontaap.com>
2017-09-10 12:35:21 +0100

    Initial project as created by Xcode

---
+++ host/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  host
+//
+//  Created by Rik Watson on 09/09/2017.
+//  Copyright © 2017 Rik Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
